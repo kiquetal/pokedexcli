0c92f0edf6d486420188f9364f989d507f820038
kiquetal <kiquetal@gmail.com>
2024-12-08 10:22:34 -0300

    Starting the main loop

--- main.go
+++ main.go
@@ -1,5 +1,66 @@
 package main
-import "fmt"
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
-fmt.Println("Hello World!")
+
+	for {
+		// Display the prompt
+		fmt.Print("pokedexcli> ")
+		//using newScanner
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		text := scanner.Text()
+		allCommands := getCommands()
+		command, ok := allCommands[text]
+		if !ok {
+			fmt.Println("Command not found")
+			continue
+		}
+		err := command.callback()
+		if err != nil {
+			fmt.Println("Error executing command")
+		}
+
+	}
+
+}
+
+func getCommands() map[string]cliCommand {
+
+	commands := map[string]cliCommand{
+		"help": cliCommand{
+			name:        "help",
+			description: "Display a help help message",
+			callback:    helpCommand,
+		},
+		"exit": cliCommand{
+			name:        "exit",
+			description: "Exit the program",
+			callback:    exitCommand,
+		},
+	}
+
+	return commands
+}
+
+func helpCommand() error {
+	fmt.Printf("Welcome to the Pokedex !\n help: Display a help message\n exit: Exit the program\n")
+	return nil
+}
+
+func exitCommand() error {
+	fmt.Println("Goodbye!")
+	os.Exit(0)
+	return nil
 }
